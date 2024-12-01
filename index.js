8b69747025f3ec40618de9b8e4e8b23e9af161cb
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2024-12-01 20:34:55 +0300

    Completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare a variable in global scope called customerName
+var customerName = 'bob';
+
+// Function that uppercases the customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function that declares a variable bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // This will be declared in global scope
+}
+
+// Function that overwrites the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // This will overwrite the global bestCustomer
+}
+
+// Declare a constant in global scope called leastFavoriteCustomer
+const leastFavoriteCustomer = 'alice';
+
+// Function that attempts to change the leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob'; // This will cause an error because leastFavoriteCustomer is a constant
+}
